ed609bdb93e4eba31f9185007d0bf5ac20cdb659
Nathan Grant <nathan.grant@timmons.com>
2016-03-01 15:02:47 -0500

    Nothing changed.

--- js/main.js
+++ js/main.js
@@ -2,25 +2,63 @@ import React from "react";
 import {render} from 'react-dom';
 
 class Main extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            clickCount: 0,
-        };
-    }
-    render() {
-        return <div>
-            <a href="" onClick={this.handleClick.bind(this)}>Clicks: {this.state.clickCount}</a>
-        </div>;
-    }
-    handleClick(event) {
-        event.preventDefault();
-        this.setState({
-            clickCount: this.state.clickCount + 1,
-        });
-    }
+	constructor(oProps) {
+		super(oProps);
+		this.handleClick = this.handleClick.bind(this);
+		this.state = {
+			clickCount: 0,
+		};
+	}
+	render() {
+		return <div>
+			<a href="" onClick={this.handleClick}>Clicks Player: {this.state.clickCount}</a>
+			<ListTest listItems={this.props.listItems}/>
+		</div>;
+	}
+	handleClick(event) {
+		event.preventDefault();
+		this.setState({
+			clickCount: this.state.clickCount + 1,
+		});
+	}
 }
 
+class ListTest extends React.Component
+{
+	constructor(oProps)
+	{
+		super(oProps);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	render()
+	{
+		var aItems = this.props.listItems.map(
+			function(oI)
+			{
+				return(
+					<div className="list-item">{oI.name}</div>
+				)
+			}
+		)
+
+		return (
+			<div className="items">{aItems}</div>
+		)
+	}
+
+	handleClick(event)
+	{
+		event.preventDefault();
+	}
+}
+
+var aData =
+[
+	{id:"r0", name:"Bob"},
+	{id:"r1", name:"Doug"}
+]
+
 render((
-    <Main/>
+	<Main listItems={aData}/>
 ), document.getElementById('main'));
