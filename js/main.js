f2a4492f18bdd080954a4e21cb56a52e417376b9
Nathan Grant <nathan.grant@timmons.com>
2016-03-02 17:06:35 -0500

    Form Monkeying, fat arrow, Proptypes

--- js/main.js
+++ js/main.js
@@ -2,25 +2,31 @@ import React from "react";
 import {render} from 'react-dom';
 
 class Main extends React.Component {
-	constructor(oProps) {
+	constructor(oProps)
+	{
 		super(oProps);
-		this.handleClick = this.handleClick.bind(this);
+		// this.handleClick = this.handleClick.bind(this);
 		this.state = {
-			clickCount: 0,
+			clickCount: 0
 		};
-	}
-	render() {
+	};
+
+	render()
+	{
 		return <div>
-			<a href="" onClick={this.handleClick}>Clicks Player: {this.state.clickCount}</a>
+			<a href="" onClick={(e)=>this.handleClick(e)}>Clicks Player: {this.state.clickCount}</a>
+			{/*<a href="" onClick={this.handleClick}>Clicks Player: {this.state.clickCount}</a>*/}
 			<ListTest listItems={this.props.listItems}/>
 		</div>;
-	}
-	handleClick(event) {
+	};
+
+	handleClick(event)
+	{
 		event.preventDefault();
 		this.setState({
 			clickCount: this.state.clickCount + 1,
 		});
-	}
+	};
 }
 
 class ListTest extends React.Component
@@ -37,13 +43,17 @@ class ListTest extends React.Component
 			function(oI)
 			{
 				return(
-					<div className="list-item">{oI.name}</div>
+					<li key={oI.key} className="list-item">
+						<h3>{oI.name}</h3>
+						<a href={oI.email}>{oI.email}</a>
+						<p>{oI.description}</p>
+					</li>
 				)
 			}
 		)
 
 		return (
-			<div className="items">{aItems}</div>
+			<ul className="items">{aItems}</ul>
 		)
 	}
 
@@ -52,14 +62,10 @@ class ListTest extends React.Component
 		event.preventDefault();
 	}
 }
-
-var aData =
-[
-	{id:"r0", name:"Bob"},
-	{id:"r1", name:"Doug"}
-];
-
-var oNewContact = {name:"", description:"", email:""};
+ListTest.propTypes =
+{
+	listItems:React.PropTypes.array.isRequired
+}
 
 class ContactForm extends React.Component
 {
@@ -81,11 +87,46 @@ class ContactForm extends React.Component
 	}
 }
 
-ContactForm.propTypes = {contact:React.PropTypes.object.isRequired};
+ContactForm.propTypes =
+{
+	contact:React.PropTypes.object.isRequired,
+};
+
+class MainView extends React.Component
+{
+	constructor(oProps)
+	{
+		super(oProps);
+	}
+
+	render()
+	{
+		return (
+			<div className={this.props.cssName}>
+				<Main listItems={this.props.contacts}/>
+				<ContactForm contact={this.props.newContact}/>
+			</div>
+		);
+	}
+}
+
+MainView.propTypes =
+{
+	contacts:React.PropTypes.array.isRequired,
+	newContact:React.PropTypes.object.isRequired,
+	cssName:React.PropTypes.string.isRequired
+}
+
+var aData =
+[
+	{key:"r0", name:"Bob", email:"bob@bob.com", description:"Makes the bacon"},
+	{key:"r1", name:"Doug", email:"doug@bob.com"},
+	{key:"r2", name:"Ain't having it"},
+];
+
+var oNewContact = {name:"", description:"", email:""};
 
 render((
-	<div>
-		<Main listItems={aData}/>
-		<ContactForm contact={oNewContact}/>
-	</div>
+		<MainView contacts={aData} newContact={oNewContact} cssName="got-monkey"/>
+
 ), document.getElementById('main'));
