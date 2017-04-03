0e927297f3bfb9b1cb44b45b00e9caeaeac45ec6
Nathan Grant <apeling@gmail.com>
2017-04-03 17:30:19 -0400

    Code clean, add compact view mode.

--- js/main.js
+++ js/main.js
@@ -1,58 +1,99 @@
 import React from "react";
 import {render} from 'react-dom';
 
-class Main extends React.Component
-{
-	constructor(oProps)
-	{
-		super(oProps);
-		// this.handleClick = this.handleClick.bind(this);
-		this.state = {
-			clickCount: 0
-		};
+let oReq = new XMLHttpRequest();
+oReq.onload = (e)=>{
+	let oData = JSON.parse(e.currentTarget.responseText);
+	let aEvents = oData["Events Main"]
+	let oStuff = {
+		EventName:{},
+		EventNameArray:[],
+		EventStartDate:{},
+		EventStartTime:{},
+		EventCategory:{}
 	};
 
-	render()
+	for (let oEvent of aEvents)
 	{
-		return <div>
-			<a href="" onClick={(e)=>this.handleClick(e)}>Clicks Player: {this.state.clickCount}</a>
-			{/*<a href="" onClick={this.handleClick}>Clicks Player: {this.state.clickCount}</a>*/}
-			<ListTest listItems={this.props.listItems} filter={this.props.filter}/>
-		</div>;
-	};
+				
+		for(let sProp in oEvent)
+		{
+			//Turning presentation data in filterable categorical data with useable field names. Removing spaces and carriage returns. 
+			const sNew = sProp.replace(new RegExp(" ", 'g'), "").replace(/[\n\r]/g, '');
 
-	handleClick(event)
-	{
-		event.preventDefault();
-		this.setState({
-			clickCount: this.state.clickCount + 1,
-		});
-	};
-}
+			//Copy data to the new scrubbed attribute name, delete the previous.
+			oEvent[sNew] = oEvent[sProp];
+			delete oEvent[sProp];
 
-class ListTest extends React.Component
-{
-	constructor(oProps)
-	{
-		super(oProps);
-		this.handleClick = this.handleClick.bind(this);
+			//Build some filter objects. Magic string to get and sort event names.
+			if(oStuff[sNew] !== null && oStuff[sNew] !== undefined && oStuff[sNew][oEvent[sNew]] == null)
+			{
+				oStuff[sNew][oEvent[sNew]] = true;
+
+				if (sNew == "EventName")
+				{
+					oStuff.EventNameArray.push(oEvent[sNew])
+				}
+			}
+		}
 	}
 
-	render()
+	//So hacky.
+	oStuff.EventNameArray = oStuff.EventNameArray.sort();
+	oStuff.EventName = {};
+
+	for (let sEN of oStuff.EventNameArray)
 	{
-		const oFilter = this.props.filter;
-		let sCategory = oFilter !== null && oFilter.category !== null ? oFilter.category : null;
-		let sValue = oFilter !== null && oFilter.value !== null ? oFilter.value : null;
-		var aItems = this.props.listItems.map(
-			function(oI)
+		oStuff.EventName[sEN] = true;
+	}
+
+	delete oStuff.EventNameArray;
+	let bCurrentCompact = false;
+	let sCurrentCategory = null;
+	let sCurrentValue = null;
+
+	let fCompact = function (bCompact){
+		bCurrentCompact = bCompact;
+		fRenderEvents(sCurrentCategory, sCurrentValue)
+	}
+
+	let fRenderEvents = function (sCategory, sValue){
+		sCurrentCategory = sCategory;
+		sCurrentValue = sValue;
+		render((<EventList events={oData["Events Main"]} filter={{category:sCategory, value:sValue}} compactView={bCurrentCompact}/>), document.getElementById('main'));
+	}
+
+	render((<FilterView filters={oStuff} onChangeCallback={fRenderEvents} onCompactUpdate={fCompact} cssName="got-monkey"/>), document.getElementById('filter-area'));
+	fRenderEvents();
+};
+oReq.open("get", "origins.json", true);
+oReq.send();
+
+//React Stateless Functional Component
+const EventList = ({events, filter, compactView}) => {
+	let sCategory = filter !== null && filter.category !== null ? filter.category : null;
+	let sValue = filter !== null && filter.value !== null ? filter.value : null;
+	
+	var aItems = events.map(
+		function(oI)
+		{
+			let bAllow = true;
+			if(sCategory !== null && sValue !== null)
 			{
-				let bAllow = true;
-				if(sCategory !== null && sValue !== null)
+				bAllow = oI[sCategory] === sValue;
+			}
+
+			if (bAllow)
+			{
+				if(compactView)
 				{
-					bAllow = oI[sCategory] === sValue;
+					return(
+						<li className="list-item">
+							<span><b>{oI["EventName"]}</b> | <span>{oI["EventStartDate"]} : {oI["EventStartTime"]} ({oI["EventDuration"]} hours)</span></span>
+						</li>
+					)
 				}
-				if (bAllow)
-				{
+				else{
 					return(
 						<li className="list-item">
 							<span><b>{oI["EventName"]}</b> | <span>{oI["EventStartDate"]} : {oI["EventStartTime"]} ({oI["EventDuration"]} hours)</span></span>
@@ -60,67 +101,22 @@ class ListTest extends React.Component
 							<div><b>Manufacturer:</b> {oI["GameManufacturer"]}| <b>System:</b> {oI["GameSystem"]} | <b>Host:</b> {oI["HostingCompanyorClub"]}</div>
 							<p>{oI["FeatureTextDescription"]}</p>
 						</li>
-					)
+					)	
 				}
 			}
-		)
+		}
+	)
 
-		return (
-			<ul className="items">{aItems}</ul>
-		)
-	}
+	return (<ul className="items">{aItems}</ul>);
+};
 
-	handleClick(event)
-	{
-		event.preventDefault();
-	}
-}
-ListTest.propTypes =
+EventList.propTypes =
 {
-	listItems:React.PropTypes.array.isRequired,
-	filter:React.PropTypes.object
+	events:React.PropTypes.array.isRequired,
+	filter:React.PropTypes.object,
+	compactView:React.PropTypes.bool
 }
 
-// class ContactForm extends React.Component
-// {
-// 	constructor(oProps)
-// 	{
-// 		super(oProps);
-// 	}
-
-// 	render()
-// 	{
-// 		return (
-// 			<form>
-// 				<input type="text" placeholder="Name (req)" value={this.props.contact.name}></input>
-// 				<input type="text" placeholder="Email" value={this.props.contact.email}></input>
-// 				<textarea placeholder="description" value={this.props.contact.description}></textarea>
-// 				<button type="submit">Add Contact</button>
-// 			</form>
-// 		)
-// 	}
-// }
-
-// ContactForm.propTypes =
-// {
-// 	contact:React.PropTypes.object.isRequired,
-// };
-//Stateless component
-// const DynamicSelect = ({options}) =>
-// {
-// 	const handleChange = (event) =>
-// 	{
-// 		console.log("I want my baby back baby back baby back", event, this)
-// 	}
-	
-// 	let aOptions = [<option value={null}>Select a Filter</option>];
-// 	for(let sProp in options)
-// 	{
-// 		aOptions.push(<option value={sProp}>{sProp}</option>)
-// 	}
-
-// 	return(<select onChange={handleChange}>{aOptions}</select>)
-// }
 class DynamicSelect extends React.Component
 {
 	constructor(oProps)
@@ -164,6 +160,15 @@ class FilterView extends React.Component
 	constructor(oProps)
 	{
 		super(oProps);
+		this.handleCompact = this.handleCompact.bind(this)
+	}
+
+	handleCompact(e) {
+		console.log("CHeck box change", e.target.checked)
+		this.props.onCompactUpdate(e.target.checked);
+		// let sValue = e.target.value === "null" ? null : e.target.value;
+		// this.state.value = sValue;
+		// this.props.onChangeCallback(this.props.category, sValue)
 	}
 
 	render()
@@ -174,7 +179,7 @@ class FilterView extends React.Component
 			aSelects.push(<DynamicSelect onChangeCallback={this.props.onChangeCallback} category={sProp} options={this.props.filters[sProp]}/>)
 		}
 		return (
-			<div className={this.props.cssName}>{aSelects}</div>
+			<div className={this.props.cssName}>{aSelects}<input type="checkBox" onChange={this.handleCompact}></input></div>
 		);
 	}
 }
@@ -182,92 +187,6 @@ FilterView.propTypes =
 {
 	cssName:React.PropTypes.string.isRequired,
 	filters:React.PropTypes.object.isRequired,
-	onChangeCallback:React.PropTypes.func.isRequired
+	onChangeCallback:React.PropTypes.func.isRequired,
+	onCompactUpdate:React.PropTypes.func.isRequired
 }
-
-class MainView extends React.Component
-{
-	constructor(oProps)
-	{
-		super(oProps);
-	}
-
-	render()
-	{
-		return (
-			<div className={this.props.cssName}>
-				<Main listItems={this.props.eventData} filter={this.props.filter}/>
-			</div>
-		);
-	}
-}
-
-MainView.propTypes =
-{
-	eventData:React.PropTypes.array.isRequired,
-	cssName:React.PropTypes.string.isRequired,
-	filter:React.PropTypes.object
-}
-
-let oReq = new XMLHttpRequest();
-oReq.onload = (e)=>{
-	let oData = JSON.parse(e.currentTarget.responseText);
-	let aEvents = oData["Events Main"]
-	let oStuff = {
-		EventName:{},
-		EventNameArray:[],
-		EventStartDate:{},
-		EventStartTime:{},
-		EventCategory:{}
-	};
-	let oGather = [
-		{id:"name", field:"EventName"},
-		{id:"date", field:"EventStartDate"},
-		{id:"start", field:"EventStartTime"},
-		{id:"type", field:"EventCategory"},
-	]
-	for (let oEvent of aEvents)
-	{
-				
-		for(let sProp in oEvent)
-		{
-			const sNew = sProp.replace(new RegExp(" ", 'g'), "").replace(/[\n\r]/g, '');
-			oEvent[sNew] = oEvent[sProp]
-			delete oEvent[sProp];
-			if(oStuff[sNew] !== null && oStuff[sNew] !== undefined && oStuff[sNew][oEvent[sNew]] == null)
-			{
-				oStuff[sNew][oEvent[sNew]] = true;
-				if (sNew == "EventName")
-				{
-					oStuff.EventNameArray.push(oEvent[sNew])
-				}
-			}
-		}
-	}
-
-	oStuff.EventNameArray = oStuff.EventNameArray.sort()
-	oStuff.EventName = {};
-	for (let sEN of oStuff.EventNameArray)
-	{
-		oStuff.EventName[sEN] = true;
-	}
-
-	delete oStuff.EventNameArray;
-	// console.log("oStuff", oStuff)
-	// console.log("oData", oData)
-
-	let fRenderMain = function (sCategory, sValue){
-		render((
-			<MainView eventData={oData["Events Main"]} filter={{category:sCategory, value:sValue}} cssName="got-monkey"/>
-
-		), document.getElementById('main'));
-	}
-
-	render((
-		<FilterView filters={oStuff} onChangeCallback={fRenderMain} cssName="got-monkey"/>
-
-	), document.getElementById('filter-area'));
-	fRenderMain();
-};
-oReq.open("get", "origins.json", true);
-oReq.send();
