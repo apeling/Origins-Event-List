b374fcf7b2768989c0444406f9099cab7f073504
Nathan Grant <apeling@gmail.com>
2017-04-04 18:01:03 -0400

    Logic to apply multiple filters.

--- js/main.js
+++ js/main.js
@@ -49,18 +49,14 @@ oReq.onload = (e)=>{
 
 	delete oStuff.EventNameArray;
 	let bCurrentCompact = false;
-	let sCurrentCategory = null;
-	let sCurrentValue = null;
 
-	let fCompact = function (bCompact){
+	let fCompact = function (bCompact, oFilters){
 		bCurrentCompact = bCompact;
-		fRenderEvents(sCurrentCategory, sCurrentValue)
+		fRenderEvents(oFilters)
 	}
 
-	let fRenderEvents = function (sCategory, sValue){
-		sCurrentCategory = sCategory;
-		sCurrentValue = sValue;
-		render((<EventList events={oData["Events Main"]} filter={{category:sCategory, value:sValue}} compactView={bCurrentCompact}/>), document.getElementById('main'));
+	let fRenderEvents = function (oFilters){
+		render((<EventList events={oData["Events Main"]} filter={oFilters} compactView={bCurrentCompact}/>), document.getElementById('main'));
 	}
 
 	render((<FilterView filters={oStuff} onChangeCallback={fRenderEvents} onCompactUpdate={fCompact} cssName="got-monkey"/>), document.getElementById('filter-area'));
@@ -71,16 +67,45 @@ oReq.send();
 
 //React Stateless Functional Component
 const EventList = ({events, filter, compactView}) => {
-	let sCategory = filter !== null && filter.category !== null ? filter.category : null;
-	let sValue = filter !== null && filter.value !== null ? filter.value : null;
+	if (filter === null || filter === undefined)
+	{
+		filter = {};
+	}
+	let oFilter = filter;
+	if (filter.category !== null)
+	{
+		let sCategory = filter !== null && filter.category !== null ? filter.category : null;
+		let sValue = filter !== null && filter.value !== null ? filter.value : null;
+		let oTemp = {};
+		oTemp[sCategory] = sValue;
+		filter = oTemp;
+	}
 	
 	var aItems = events.map(
-		function(oI)
+		(oI)=>
 		{
+			let nFilters = 0;
+			let nPass = 0;
 			let bAllow = true;
-			if(sCategory !== null && sValue !== null)
+
+			for(let sCat in oFilter)
 			{
-				bAllow = oI[sCategory] === sValue;
+				if (sCat !== null && sCat !== undefined && oFilter[sCat] !== null && oFilter[sCat] !== undefined)
+				{
+					nFilters++;
+					if (oI[sCat] === oFilter[sCat])
+					{
+						nPass++;
+					}
+				}
+			}
+
+			if(nFilters > 0)
+			{
+				if (nPass < nFilters)
+				{
+					bAllow = false;
+				}
 			}
 
 			if (bAllow)
@@ -117,6 +142,59 @@ EventList.propTypes =
 	compactView:React.PropTypes.bool
 }
 
+class FilterView extends React.Component
+{
+	constructor(oProps)
+	{
+		super(oProps);
+
+		this.state = {};
+		for(let sProp in this.props.filters)
+		{
+			this.state[sProp] = null;
+		}
+
+		this.handleCompact = this.handleCompact.bind(this);
+		this.handleFilter = this.handleFilter.bind(this);
+		this.clearFilter = this.clearFilter.bind(this);
+
+	}
+
+	handleCompact(e) {
+		this.props.onCompactUpdate(e.target.checked, this.state);
+	}
+
+	handleFilter(sCategory, sValue) {
+		this.state[sCategory] = sValue
+		this.props.onChangeCallback(this.state);
+	}
+	
+	clearFilter() {
+		console.log("Clear the filters")
+		this.state = {}
+		this.props.onChangeCallback(this.state);
+	}
+
+	render()
+	{
+		let aSelects = [];
+		for(let sProp in this.props.filters)
+		{
+			aSelects.push(<DynamicSelect onChangeCallback={this.handleFilter} category={sProp} options={this.props.filters[sProp]}/>)
+		}
+		return (
+			<div className={this.props.cssName}>{aSelects}<input type="checkBox" onChange={this.handleCompact}></input><div onClick={this.clearFilter} className={this.props.cssName}>Clear</div></div>
+		);
+	}
+}
+FilterView.propTypes =
+{
+	cssName:React.PropTypes.string.isRequired,
+	filters:React.PropTypes.object.isRequired,
+	onChangeCallback:React.PropTypes.func.isRequired,
+	onCompactUpdate:React.PropTypes.func.isRequired
+}
+
 class DynamicSelect extends React.Component
 {
 	constructor(oProps)
@@ -130,7 +208,7 @@ class DynamicSelect extends React.Component
 
 	handleChange(e) {
 		let sValue = e.target.value === "null" ? null : e.target.value;
-		this.state.value = sValue;
+		this.setState({value:sValue});
 		this.props.onChangeCallback(this.props.category, sValue)
 	}
 
@@ -153,36 +231,4 @@ DynamicSelect.propTypes =
 	options:React.PropTypes.object.isRequired,
 	category:React.PropTypes.string.isRequired,
 	onChangeCallback:React.PropTypes.func.isRequired
-}
-
-class FilterView extends React.Component
-{
-	constructor(oProps)
-	{
-		super(oProps);
-		this.handleCompact = this.handleCompact.bind(this)
-	}
-
-	handleCompact(e) {
-		this.props.onCompactUpdate(e.target.checked);
-	}
-
-	render()
-	{
-		let aSelects = [];
-		for(let sProp in this.props.filters)
-		{
-			aSelects.push(<DynamicSelect onChangeCallback={this.props.onChangeCallback} category={sProp} options={this.props.filters[sProp]}/>)
-		}
-		return (
-			<div className={this.props.cssName}>{aSelects}<input type="checkBox" onChange={this.handleCompact}></input></div>
-		);
-	}
-}
-FilterView.propTypes =
-{
-	cssName:React.PropTypes.string.isRequired,
-	filters:React.PropTypes.object.isRequired,
-	onChangeCallback:React.PropTypes.func.isRequired,
-	onCompactUpdate:React.PropTypes.func.isRequired
 }
